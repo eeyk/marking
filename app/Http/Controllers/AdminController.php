25c0cc708acc9b2c30c1d5bd0d15b12597dc6593
scut_yk <scut_yk@foxmail.com>
2017-11-13 22:57:44 +0800

    view2

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -50,7 +50,7 @@ class AdminController extends Controller
 
     public function getCreateActivity()
     {
-        return view('createActivity');
+        return view('createactivity');
     }
 
     public function showActivity($id)
